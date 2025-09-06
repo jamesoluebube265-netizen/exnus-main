494de5f0f4468d2bb8d5eccf75e5a885c2cb5004
Oluebube James <jamesoluebube265@gmail.com>
2025-09-06 06:36:48 +0000

    Try fixing this error: `Console Error: `DialogContent` requires a `Dialo

--- src/components/ui/sheet.tsx
+++ src/components/ui/sheet.tsx
@@ -4,6 +4,8 @@ import * as React from "react"
 import * as SheetPrimitive from "@radix-ui/react-dialog"
 import { cva, type VariantProps } from "class-variance-authority"
 import { X } from "lucide-react"
+import * as VisuallyHidden from "@radix-ui/react-visually-hidden";
+
 
 import { cn } from "@/lib/utils"
 
@@ -69,6 +71,10 @@ const SheetContent = React.forwardRef<
         <X className="h-4 w-4" />
         <span className="sr-only">Close</span>
       </SheetPrimitive.Close>
+       <VisuallyHidden.Root>
+        <SheetPrimitive.Title />
+        <SheetPrimitive.Description />
+      </VisuallyHidden.Root>
     </SheetPrimitive.Content>
   </SheetPortal>
 ))
