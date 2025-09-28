9594a160fd341c134fdd7d3a54c003f4219df8ad
Oluebube James <jamesoluebube265@gmail.com>
2025-09-28 21:45:02 +0000

    this menu side should not scroll with page  (_for element <DesktopNavLin

--- src/components/layout/client-layout.tsx
+++ src/components/layout/client-layout.tsx
@@ -14,9 +14,9 @@ export default function ClientLayout({
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <div className="flex min-h-screen bg-background">
+    <div className="min-h-screen bg-background">
       <Sidebar isMobileMenuOpen={isMobileMenuOpen} setMobileMenuOpen={setMobileMenuOpen} />
-      <div className="flex-1 flex flex-col">
+      <div className="flex flex-col md:pl-20">
         <NewHeader onMenuClick={() => setMobileMenuOpen(true)} />
         <main className="flex-1 p-4 sm:px-6 md:gap-8">
           {children}
